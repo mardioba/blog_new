8a461accc97215edab8146f7c03e98ae4a3018d3
Mardio Moreira <mardioba@gmail.com>
2024-04-12 11:04:18 -0300

    :Blog v11 12-04-2024 as 11:03

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from post.models import Post, Category
+from django.core.paginator import Paginator
 def index(request):
   post_random = Post.objects.order_by('?')[:4]
   post_latest = Post.objects.order_by('id')[:3]
@@ -14,9 +15,13 @@ def blog(request):
   posts = Post.objects.all()
   post_latest = Post.objects.order_by('id')[:3]
   post_category = Category.objects.order_by('title').all()
+  paginator = Paginator(posts, 2)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+  
   context = {
-    'posts': posts,
+    'page_obj': page_obj,
     'post_category': post_category,
-    'post_latest': post_latest
+    'post_latest': post_latest,
   }
   return render(request, 'pages/blog.html', context)
